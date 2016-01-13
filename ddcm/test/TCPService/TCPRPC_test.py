ba386106c5f6e3213e857a5d7211773f8017c7c8
SkyZH <iSkyZH@gmail.com>
2016-01-13 21:13:35 +0800

    change test style

--- ddcm/test/TCPService/TCPRPC_test.py
+++ ddcm/test/TCPService/TCPRPC_test.py
@@ -11,33 +11,54 @@ import ddcm
 from .. import const
 
 class TCPRPCTest(unittest.TestCase):
+    def TestCase(func):
+        def _deco(*args, **kwargs):
+            config = ddcm.utils.load_config("config.json")
 
-    def test_pack_ping(self):
-        config = ddcm.utils.load_config("config.json")
+            loop = asyncio.get_event_loop()
+            loop.set_debug(config['debug']['asyncio']['enabled'])
 
-        loop = asyncio.get_event_loop()
-        loop.set_debug(config['debug']['asyncio']['enabled'])
+            service = ddcm.Service(config, loop)
+            echo = ddcm.utils.get_echo_bytes()
 
-        service = ddcm.Service(config, loop)
-        echo = ddcm.utils.get_echo_bytes()
+            rsock, wsock = socketpair()
 
-        rsock, wsock = socketpair()
+            reader, writer = loop.run_until_complete(
+                asyncio.open_connection(sock=rsock, loop=loop)
+            )
 
-        reader, writer = loop.run_until_complete(
-            asyncio.open_connection(sock=rsock, loop=loop)
-        )
+            kwargs = {
+                'reader': reader,
+                'wsock': wsock,
+                'tcpService': service.tcpService,
+                'echo': echo,
+                'loop': loop
+            }
 
+            ret = func(*args, **kwargs)
+
+            writer.close()
+            wsock.close()
+
+            return ret
+        return _deco
+
+    @TestCase
+    def test_pack_ping(self, loop, reader, wsock, tcpService, echo):
         wsock.send(
-            service.tcpService.rpc.pack_ping(service.tcpService.node, echo)
+            tcpService.rpc.pack_ping(
+                tcpService.node,
+                tcpService.server.remote,
+                echo
+            )
         )
 
         _command, _echo, _remoteNode, _data = loop.run_until_complete(
             asyncio.ensure_future(
-                service.tcpService.rpc.read_command(reader)
+                tcpService.rpc.read_command(reader)
             )
         )
-        writer.close()
-        wsock.close()
+
 
         self.assertEqual(_command, ddcm.const.kad.command.PING)
         self.assertEqual(echo, _echo)
